707b4c003e33810634272bf3710a0a4d3915b3c1
Mike DiGioia <digioia.mike@gmail.com>
2015-12-10 17:10:09 -0500

    WIP:: Broken ISOMORPHIC build

--- allow-jade-tags.js
+++
@@ -1,9 +0,0 @@
-import riot from 'riot';
-import fs from 'fs';
-
-const compile = path => riot.compile(fs.readFileSync(path, 'utf8'), { template: 'jade', type:'babel',  presets: ["es2015"]});
-const loadTag = (path, options) => (
-  module._compile(`var riot = require(process.env.RIOT || "riot/riot.js");module.exports = ${compile(path)}`, path)
-);
-export {compile};
-export default (path) => loadTag(path);
--- client/app.js
+++
@@ -1,9 +0,0 @@
-import riot from 'riot'
-
-import TodoStore from './store.js';
-import dispatcher from './dispatcher.js';
-
-let todoStore = new TodoStore(dispatcher);
-dispatcher.addStore(todoStore);
-
-riot.mount('*', {store: todoStore})
